59179b014cd46bd5ebf6a43f66572b665ca3d652
Matheus Venceslau <matheushlvenceslau@gmail.com>
2025-04-05 16:51:25 -0300

    feat: Alterações na Classe instrutor e Atualizações na Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import models.Academia;
+import models.Cliente;
+import models.Instrutor;
+
 public class Main {
     public static void main(String[] args) {
+        
+        Academia academia = new Academia("Java Senai Fitness");
 
+        Cliente clienteSenai01 = new Cliente("Paulo", 20, "Plus++");
+        Cliente clienteSenai02 = new Cliente("Pedro", 40, "Basico");
+        Cliente clienteSenai03 = new Cliente("Luiz", 28, "Premium");
+
+        Instrutor instrutor01 = new Instrutor("Matheus", 21, "Musculação");
+        Instrutor instrutor02 = new Instrutor("João", 21, "Pilates");
+
+        academia.adicionarPessoa(clienteSenai01);
+        academia.adicionarPessoa(clienteSenai02);
+        academia.adicionarPessoa(clienteSenai03);
+
+        academia.adicionarPessoa(instrutor01);
+        academia.adicionarPessoa(instrutor02);
+
+        academia.listarPessoas();
     }
 }
--- src/models/Instrutor.java
+++ src/models/Instrutor.java
@@ -1,6 +1,6 @@
 package models;
 
-abstract class Instrutor extends Pessoa{
+public class Instrutor extends Pessoa{
     private String especialidadeInstrutor;
 
     public Instrutor(String nome, int idade, String especialidadeInstrutor) {
@@ -13,11 +13,12 @@ abstract class Instrutor extends Pessoa{
     }
 
     @Override
-    public String toString() {
+    public String getDescricao() {
         return "Instrutor{" +
                 "especialidadeInstrutor='" + especialidadeInstrutor + '\'' +
                 ", nomePessoa='" + nomePessoa + '\'' +
                 ", idadePessoa=" + idadePessoa +
                 '}';
     }
+
 }
