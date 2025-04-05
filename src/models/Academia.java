582e08537dacb89a1191ec21d6e68fdaf4aec040
Matheus Venceslau <matheushlvenceslau@gmail.com>
2025-04-05 15:48:52 -0300

    feat: Classe Academia

--- src/models/Academia.java
+++ src/models/Academia.java
@@ -1,4 +1,25 @@
 package models;
 
+import java.util.ArrayList;
+
+// Composição e Polimorfismo
 public class Academia {
+    private String nomeAcademia;
+    private ArrayList<Pessoa> pessoas;
+
+    public Academia(String nome){
+        this.nomeAcademia = nome;
+        this.pessoas = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(Pessoa pessoa){
+        pessoas.add(pessoa);
+    }
+
+    public void listarPessoas(){
+        System.out.println("Lista de Pessoas na Academia");
+        for(Pessoa pessoa : pessoas){
+            System.out.println(pessoa.getDescricao());
+        }
+    }
 }
